946b203e35383b72da09a72ddcc846d6d7271d8d
axeratos <nikitalukianitcares@gmail.com>
2022-12-23 19:58:20 +0200

    Path to db

--- backend/app/db/session_core.py
+++ backend/app/db/session_core.py
@@ -1,5 +1,9 @@
+import pathlib
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///./storage/db_file.db')
+path = pathlib.Path(__file__).parent.joinpath("storage/db_file.db")
+
+engine = create_engine(f'sqlite:///{path}', connect_args={'check_same_thread': False}, pool_pre_ping=True)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)

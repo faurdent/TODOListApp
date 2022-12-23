9b2c6a5e1d890181c2757db20d17a5151f0568e4
axeratos <nikitalukianitcares@gmail.com>
2022-12-23 19:59:33 +0200

    Base routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-from .schemas import TaskOut
+from .dependencies import get_db
+from .models import Task
+from .schemas import TaskIn, TaskDBOut
 
 app = FastAPI()
 
@@ -10,6 +13,26 @@ async def main_page():
     return {"hello": "world"}
 
 
-@app.get("/my-tasks/", response_model=dict[int, TaskOut])
-async def get_all_tasks():
-    return {1: TaskOut(title="First", description="My first task")}
+@app.post("/my-tasks/add-task/", response_model=TaskDBOut)
+async def add_task(task: TaskIn, db: Session = Depends(get_db)):
+    task_obj = Task(**task.dict())
+    print(task_obj)
+    db.add(task_obj)
+    db.commit()
+    db.refresh(task_obj)
+    return task_obj
+
+
+@app.get("/my-tasks/{pk}", response_model=TaskDBOut)
+async def get_one_task(pk: int, db: Session = Depends(get_db)):
+    task_obj = db.query(Task).get(pk)
+    if not task_obj:
+        raise HTTPException(status_code=404, detail="Task does not exists")
+    return task_obj
+
+
+@app.get("/my-tasks/", response_model=list[TaskDBOut])
+async def get_all_tasks(db: Session = Depends(get_db)):
+    queryset = db.query(Task).all()
+    print(queryset)
+    return queryset

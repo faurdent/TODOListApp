fdae9812eb45b628a1bad12d68f212b4497f96d7
axeratos <nikitalukianitcares@gmail.com>
2022-12-26 09:39:45 +0200

    Security utils

---
+++ backend/app/core/security.py
@@ -0,0 +1,22 @@
+from datetime import timedelta, datetime, timezone
+
+from jose import jwt
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+ALGORITHM = "HS256"
+
+
+def create_access_token(subject: int, expires_delta: timedelta):
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode_data = {"subject": subject, "exp": expire}
+    return jwt.encode(to_encode_data, "8ZIOl6Rcuh4X+/oK/iArCx4qWSBkGjG3nXGcSlC0xx8=", algorithm=ALGORITHM)
+
+
+def hash_password(plain_password: str):
+    return pwd_context.hash(plain_password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)

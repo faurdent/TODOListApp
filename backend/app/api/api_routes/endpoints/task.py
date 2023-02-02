3579b8e6afccbc7d6a92263656b324adc43c0399
axeratos <nikitalukianitcares@gmail.com>
2023-02-02 13:39:41 +0200

    Routes cleaned

--- backend/app/api/api_routes/endpoints/task.py
+++ backend/app/api/api_routes/endpoints/task.py
@@ -3,19 +3,13 @@ from datetime import date
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from backend.app.api.dependencies import get_current_user, get_db
+from backend.app.api.dependencies import get_current_user, get_db, get_current_verified_user
 from backend.app.crud import task, week, day
 from backend.app.models import User
 from backend.app.schemas import TaskSchema, TaskUpdate, TaskCreate
 from backend.app.schemas.task import DaySchema
 
 router = APIRouter()
-
-
-# @router.post("/add-task/", response_model=TaskSchema)
-# async def create_task(task_obj: TaskCreate, owner: User = Depends(get_current_user), db: Session = Depends(get_db)):
-#     created_task = task.create_with_owner(db=db, obj_in=task_obj, owner_id=owner.pk)
-#     return created_task
 
 
 @router.put("/{pk}", response_model=TaskSchema)
@@ -30,7 +24,7 @@ async def update_task(
 
 
 @router.delete("/{pk}")
-async def delete_task(pk: int, owner: User = Depends(get_current_user), db: Session = Depends(get_db)):
+async def delete_task(pk: int, owner: User = Depends(get_current_verified_user), db: Session = Depends(get_db)):
     task_to_delete = task.get_with_owner(db=db, pk=pk, owner_id=owner.pk)
     if not task_to_delete:
         raise HTTPException(status_code=404, detail="Task not found")
@@ -39,17 +33,14 @@ async def delete_task(pk: int, owner: User = Depends(get_current_user), db: Sess
 
 
 @router.get("/{pk}", response_model=TaskSchema)
-async def get_task(pk: int, owner: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    # task_obj = task.get_with_owner(db=db, pk=pk, owner_id=owner.pk)
+async def get_task(pk: int, owner: User = Depends(get_current_verified_user), db: Session = Depends(get_db)):
     if task_obj := task.get_with_owner(db=db, pk=pk, owner_id=owner.pk):
         return task_obj
     raise HTTPException(status_code=404, detail="Task not found")
-    # print(task_obj)
-    # return task_obj
 
 
 @router.get("/", response_model=list[TaskSchema])
-async def get_my_tasks(owner: User = Depends(get_current_user), db: Session = Depends(get_db)):
+async def get_my_tasks(owner: User = Depends(get_current_verified_user), db: Session = Depends(get_db)):
     return task.get_all_by_owner(db=db, owner_id=owner.pk)
 
 
@@ -62,7 +53,7 @@ async def get_week(week_start: date, db: Session = Depends(get_db), owner: User 
 
 
 @router.post("/add-day/", deprecated=True)
-async def add_day(week_start: date, db: Session = Depends(get_db), owner: User = Depends(get_current_user)):
+async def add_day(week_start: date, db: Session = Depends(get_db), owner: User = Depends(get_current_verified_user)):
     current_week = week.get_week_with_owner(db=db, start_day=week_start, owner_id=owner.pk)
     day_obj = day.create_day_with_week(db=db, week_id=current_week.pk)
     print(day_obj)
@@ -73,20 +64,22 @@ async def add_day(week_start: date, db: Session = Depends(get_db), owner: User =
 async def add_task(
         task_in: TaskCreate,
         day_pk: int, db: Session = Depends(get_db),
-        owner: User = Depends(get_current_user)
+        owner: User = Depends(get_current_verified_user)
 ):
     # current_week = week.get_week_with_owner(db=db, start_day=week_start, owner_id=owner.pk)
     day_obj = day.get(db=db, pk=day_pk)
     if not day_obj:
         raise HTTPException(status_code=404, detail="Day not found")
     task_obj = task.create_task_with_day(db=db, day_id=day_pk, task_obj=task_in)
-    print(task_obj)
     return task_obj
 
 
 @router.get("/test-tasks/{week_start}", response_model=list[DaySchema])
-async def get_tasks_for_week(week_start: date, db: Session = Depends(get_db), owner: User = Depends(get_current_user)):
+async def get_tasks_for_week(
+        week_start: date,
+        db: Session = Depends(get_db),
+        owner: User = Depends(get_current_verified_user)
+):
     current_week = week.get_week_with_owner(db=db, start_day=week_start, owner_id=owner.pk)
-    # print(current_week.week_days)
     return current_week.week_days
 

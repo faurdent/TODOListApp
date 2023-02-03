504e99e342b2e9d85a0de090f22a67425ab2f1ff
axeratos <nikitalukianitcares@gmail.com>
2023-02-03 15:58:22 +0200

    Week and days managing in db updated

--- backend/app/api/api_routes/endpoints/task.py
+++ backend/app/api/api_routes/endpoints/task.py
@@ -14,7 +14,10 @@ router = APIRouter()
 
 @router.put("/{pk}", response_model=TaskSchema)
 async def update_task(
-    pk: int, task_obj: TaskUpdate, owner: User = Depends(get_current_user), db: Session = Depends(get_db)
+    pk: int,
+    task_obj: TaskUpdate,
+    owner: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
 ):
     task_to_update = task.get_with_owner(db=db, pk=pk, owner_id=owner.pk)
     if not task_to_update:
@@ -24,7 +27,11 @@ async def update_task(
 
 
 @router.delete("/{pk}")
-async def delete_task(pk: int, owner: User = Depends(get_current_verified_user), db: Session = Depends(get_db)):
+async def delete_task(
+    pk: int,
+    owner: User = Depends(get_current_verified_user),
+    db: Session = Depends(get_db),
+):
     task_to_delete = task.get_with_owner(db=db, pk=pk, owner_id=owner.pk)
     if not task_to_delete:
         raise HTTPException(status_code=404, detail="Task not found")
@@ -33,28 +40,46 @@ async def delete_task(pk: int, owner: User = Depends(get_current_verified_user),
 
 
 @router.get("/{pk}", response_model=TaskSchema)
-async def get_task(pk: int, owner: User = Depends(get_current_verified_user), db: Session = Depends(get_db)):
+async def get_task(
+    pk: int,
+    owner: User = Depends(get_current_verified_user),
+    db: Session = Depends(get_db),
+):
     if task_obj := task.get_with_owner(db=db, pk=pk, owner_id=owner.pk):
         return task_obj
     raise HTTPException(status_code=404, detail="Task not found")
 
 
 @router.get("/", response_model=list[TaskSchema])
-async def get_my_tasks(owner: User = Depends(get_current_verified_user), db: Session = Depends(get_db)):
+async def get_my_tasks(
+    owner: User = Depends(get_current_verified_user), db: Session = Depends(get_db)
+):
     return task.get_all_by_owner(db=db, owner_id=owner.pk)
 
 
 @router.get("/test-week/{week_start}")
-async def get_week(week_start: date, db: Session = Depends(get_db), owner: User = Depends(get_current_user)):
-    current_week = week.get_week_with_owner(db=db, start_day=week_start, owner_id=owner.pk)
+async def get_week(
+    week_start: date,
+    db: Session = Depends(get_db),
+    owner: User = Depends(get_current_user),
+):
+    current_week = week.get_week_with_owner(
+        db=db, start_day=week_start, owner_id=owner.pk
+    )
     if current_week:
         return current_week
     return week.create_week_with_owner(db=db, start_day=week_start, owner_id=owner.pk)
 
 
 @router.post("/add-day/", deprecated=True)
-async def add_day(week_start: date, db: Session = Depends(get_db), owner: User = Depends(get_current_verified_user)):
-    current_week = week.get_week_with_owner(db=db, start_day=week_start, owner_id=owner.pk)
+async def add_day(
+    week_start: date,
+    db: Session = Depends(get_db),
+    owner: User = Depends(get_current_verified_user),
+):
+    current_week = week.get_week_with_owner(
+        db=db, start_day=week_start, owner_id=owner.pk
+    )
     day_obj = day.create_day_with_week(db=db, week_id=current_week.pk)
     print(day_obj)
     return day_obj
@@ -62,7 +87,10 @@ async def add_day(week_start: date, db: Session = Depends(get_db), owner: User =
 
 @router.post("/add-task/{day_pk}")
 async def add_task(
-    task_in: TaskCreate, day_pk: int, db: Session = Depends(get_db), owner: User = Depends(get_current_verified_user)
+    task_in: TaskCreate,
+    day_pk: int,
+    db: Session = Depends(get_db),
+    owner: User = Depends(get_current_verified_user),
 ):
     # current_week = week.get_week_with_owner(db=db, start_day=week_start, owner_id=owner.pk)
     day_obj = day.get(db=db, pk=day_pk)
@@ -74,9 +102,11 @@ async def add_task(
 
 @router.get("/test-tasks/{week_start}", response_model=list[DaySchema])
 async def get_tasks_for_week(
-    week_start: date, db: Session = Depends(get_db),
+    week_start: date,
+    db: Session = Depends(get_db),
     # owner: User = Depends(get_current_verified_user)
 ):
     # current_week = week.get_week_with_owner(db=db, start_day=week_start, owner_id=owner.pk)
     current_week = week.get_week_with_owner(db=db, start_day=week_start, owner_id=1)
-    return current_week.week_days
+    weekdays = current_week.week_days
+    return weekdays or day.create_days_for_week(db=db, week_id=current_week.pk)
--- backend/app/crud/crud_task.py
+++ backend/app/crud/crud_task.py
@@ -26,7 +26,14 @@ class CRUDWeek(CRUDBase[Week]):
         return db_obj
 
     def get_week_with_owner(self, db: Session, start_day: date, owner_id: int):
-        return db.query(self.model).filter(self.model.start_day == start_day, self.model.owner_id == owner_id).first()
+        week_obj = (
+            db.query(self.model)
+            .filter(self.model.start_day == start_day, self.model.owner_id == owner_id)
+            .first()
+        )
+        return week_obj or self.create_week_with_owner(
+            db=db, start_day=start_day, owner_id=owner_id
+        )
 
 
 class CRUDDay(CRUDBase[Day]):
@@ -36,6 +43,15 @@ class CRUDDay(CRUDBase[Day]):
         db.commit()
         db.refresh(db_obj)
         return db_obj
+
+    def create_days_for_week(self, db: Session, week_id: int):
+        weekdays = ["Monday", "Sunday", "Wednesday", "Tuesday", "Friday"]
+        day_objs = [
+            self.model(weekday=day_name, week_id=week_id) for day_name in weekdays  # noqa
+        ]
+        db.add_all(day_objs)
+        db.commit()
+        return day_objs
 
 
 task = CRUDTask(Task)

4930af08a9e1b5eb1d5d2322beaad385996b3306
axeratos <nikitalukianitcares@gmail.com>
2023-02-16 11:19:19 +0200

    Component for specific day

---
+++ frontend/src/components/DayTasks.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: "DayTasks",
+  props: {
+    id: {type: String, required: true}
+  },
+  data() {
+    return {
+      tasks: null
+    }
+  },
+  methods: {
+    async getData() {
+      const response = await fetch(`http://localhost:8000/my-tasks/day/${this.id}`)
+      this.tasks = await response.json()
+    }
+  },
+  async created() {
+    await this.getData()
+  }
+}
+</script>
+
+<template>
+<div>{{tasks}}</div>
+</template>

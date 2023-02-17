4955e80ebe64f32b6ba3d257972f23ff19d01a56
axeratos <nikitalukianitcares@gmail.com>
2023-02-17 12:38:34 +0200

    Day Tasks component template created

--- frontend/src/components/DayTasks.vue
+++ frontend/src/components/DayTasks.vue
@@ -6,13 +6,13 @@ export default {
   },
   data() {
     return {
-      tasks: null
+      dayData: null
     }
   },
   methods: {
     async getData() {
       const response = await fetch(`http://localhost:8000/my-tasks/day/${this.id}`)
-      this.tasks = await response.json()
+      this.dayData = await response.json()
     }
   },
   async created() {
@@ -22,5 +22,17 @@ export default {
 </script>
 
 <template>
-<div>{{tasks}}</div>
+  <div v-if="dayData" class="day">
+    <div>{{ dayData }}</div>
+    <h1>{{ dayData.weekday }}</h1>
+    <ul>
+      <li v-for="task in dayData.tasks" :key="task.pk">
+        <h3>{{ task.title }}</h3>
+        <p>Description: {{ task.description || "Not provided" }}</p>
+      </li>
+    </ul>
+  </div>
+  <div v-else>
+    <h2>Loading...</h2>
+  </div>
 </template>

8f3604a4ed8eb4834464985221d0ee9c592c41a2
axeratos <nikitalukianitcares@gmail.com>
2023-02-20 10:34:15 +0200

    Views moved to special directory

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router"
 
-import MainTasksList from "@/components/MainTasksList.vue";
-import DayTasks from "@/components/DayTasks.vue";
+import MainTasksList from "@/views/MainTasksList.vue";
+import DayTasks from "@/views/DayTasks.vue";
 
 const routes = [
     {path: "/week/:weekStart?", name: "MainTasksList", component: MainTasksList, props: true},

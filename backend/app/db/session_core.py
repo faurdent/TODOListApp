5a31e0877c50ccd18a24a5efc0c555e5513f5f6d
axeratos <nikitalukianitcares@gmail.com>
2023-02-21 12:02:56 +0200

    Async engine and session creation

--- backend/app/db/session_core.py
+++ backend/app/db/session_core.py
@@ -1,14 +1,7 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-from app.core.config import config
+from app.core.config import app_config
 
-# path = pathlib.Path(__file__).parent.joinpath("storage/db_file.db")
+engine = create_async_engine(app_config.get_db_url(), echo=True)
 
-# engine = create_engine(
-#     f"sqlite:///{path}", connect_args={"check_same_thread": False}, pool_pre_ping=True
-# )
-# SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
-
-engine = create_async_engine(config.get_db_url(), echo=True)
-
-SessionLocal = AsyncSession(engine, expire_on_commit=False)
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)

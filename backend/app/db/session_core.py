67b60b574fae3ce36c949e6c4840e7e940154713
axeratos <nikitalukianitcares@gmail.com>
2023-02-23 10:32:33 +0200

    Session config changed

--- backend/app/db/session_core.py
+++ backend/app/db/session_core.py
@@ -4,4 +4,4 @@ from app.core.config import app_config
 
 engine = create_async_engine(app_config.get_db_url(), echo=True)
 
-SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False, autoflush=False)

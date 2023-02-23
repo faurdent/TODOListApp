74d3d49252fe5b9c3f9240fe9a02b75eedb6caee
axeratos <nikitalukianitcares@gmail.com>
2023-02-23 09:49:26 +0200

    Base class inherits from DeclarativeBase

--- backend/app/db/base_class.py
+++ backend/app/db/base_class.py
@@ -1,16 +1,5 @@
-from sqlalchemy import Column, Integer
-from sqlalchemy.ext.declarative import as_declarative
-from sqlalchemy.orm import declared_attr
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-@as_declarative()
-class Base:
-    pk = Column(Integer, primary_key=True, autoincrement=True)
-
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
-
-    @declared_attr
-    def __table_args__(cls):
-        return {"extend_existing": True}
+class Base(DeclarativeBase):
+    pk: Mapped[int] = mapped_column(primary_key=True)

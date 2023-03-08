e3a0c71e7a4b4d5b1747589b66671067a1e45693
axeratos <nikitalukianitcares@gmail.com>
2023-03-08 12:48:44 +0200

    Week days view rewrote using Composition API

--- frontend/src/views/MainTasksList.vue
+++ frontend/src/views/MainTasksList.vue
@@ -1,58 +1,35 @@
-<script>
+<template>
+  <div v-if="store.isDataLoaded">
+    <h1>Start of the week: {{ store.getStartDate }}</h1>
+    <the-day v-for="day in store.getWeekDays" :day-data="day"/>
+    <weeks-pagination :week-start="weekStartString"/>
+  </div>
+</template>
+
+<script setup>
+import {useWeekDataStore} from "@/store/weekData";
+import {computed} from "vue";
 import dateFormat from "dateformat";
+
 import TheDay from "@/components/WeekTasksComponents/TheDay.vue";
 import WeeksPagination from "@/components/WeeksPagination.vue";
 
-export default {
-  name: "MainTasksList",
-  components: {TheDay, WeeksPagination},
-  props: {
-    weekStart: {
-      type: String
+
+const store = useWeekDataStore()
+
+const props = defineProps(["weekStart"])
+
+const weekStartString = computed(() => {
+  if (!props.weekStart) {
+    const nowDate = new Date()
+    const nowDay = nowDate.getDay() || 7
+    if (nowDay !== 1) {
+      nowDate.setHours(-24 * (nowDay - 1))
     }
-  },
-  data() {
-    return {
-      weekData: []
-    }
-  },
-  methods: {
-    async getData() {
-      fetch(`http://localhost:8000/my-tasks/test-tasks/${this.weekStartString}`)
-          .then(res1 => res1.json())
-          .then(json => this.weekData = json)
-    },
-    async deleteTask(day, toDelete) {
-      const taskIndex = day.tasks.findIndex(task => task.pk === toDelete)
-      await fetch(`http://localhost:8000/my-tasks/${toDelete}`, {method: "DELETE"})
-      day.tasks.splice(taskIndex, 1)
-    },
-  },
-  computed: {
-    weekStartString() {
-      if (!this.weekStart) {
-        const nowDate = new Date()
-        const nowDay = nowDate.getDay() || 7
-        if (nowDay !== 1) {
-          nowDate.setHours(-24 * (nowDay - 1))
-        }
-        return dateFormat(nowDate, "yyyy-mm-dd")
-      }
-      return this.weekStart
-    },
-  },
-  created() {
-    this.getData()
+    return dateFormat(nowDate, "yyyy-mm-dd")
   }
+  return props.weekStart
+})
 
-}
+store.fetchData(weekStartString.value)
 </script>
-
-<template>
-  <div>
-    {{ this.weekData }}
-    {{ this.weekStart }}
-    <the-day v-for="day in weekData" :day-data="day"/>
-  </div>
-  <weeks-pagination :week-start="weekStartString"/>
-</template>

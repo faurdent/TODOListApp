0d76aacf6c25cfa5717e13a4d6d386130159f700
axeratos <nikitalukianitcares@gmail.com>
2023-03-08 12:32:56 +0200

    Router changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,21 @@
 import {createRouter, createWebHistory} from "vue-router"
 
+import {useWeekDataStore} from "@/store/weekData";
+
 import MainTasksList from "@/views/MainTasksList.vue";
-import DayTasks from "@/views/DayTasks.vue";
 
 const routes = [
-    {path: "/week/:weekStart?", name: "MainTasksList", component: MainTasksList, props: true},
-    {path: "/day/:id", name: "DayTasks", component: DayTasks, props: true},
+    {
+        path: "/week/:weekStart",
+        name: "MainTasksList",
+        component: MainTasksList,
+        props: true,
+        beforeEnter: (to, from) => {
+            const store = useWeekDataStore()
+            store.fetchData(to.params.weekStart)
+        },
+    },
+    // {path: "/week/:weekStart/:weekdaySlug", name: "DayTasks", component: DayTasks, props: true},
 ]
 
 const router = createRouter({

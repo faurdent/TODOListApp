9d2aa12de0af74e958073f01541650f7c7bdf051
axeratos <nikitalukianitcares@gmail.com>
2023-03-08 12:46:20 +0200

    Modal window for task creation

--- frontend/src/components/WeekTasksComponents/TheDay.vue
+++ frontend/src/components/WeekTasksComponents/TheDay.vue
@@ -1,44 +1,69 @@
 <template>
   <div style="transition: margin-top; transition-duration: 0.25s">
-    <router-link :to="`/day/${dayData.pk}`">
-      <h2 style="display: inline;">{{ dayData.weekday }}</h2>
-    </router-link>
+    <h2 style="display: inline;">{{ dayData.weekday }}</h2>
+    <button @click="toggleForm">Add Task</button>
+    <the-modal-add @close="toggleForm" :weekday="dayData.weekday" v-if="isModalVisible"/>
+    <div v-if="isModalVisible" id="overlay"></div>
     <ul>
-      <li v-for="task in tasks">
-        <the-task :taskData="task" @delete="deleteTask"/>
+      <li v-for="(task, index) in tasks">
+        <the-task :dayPk="dayData.pk" :index="index" :taskData="task"/>
       </li>
     </ul>
   </div>
 </template>
 
-<script>
-import TheTask from "@/components/WeekTasksComponents/TheTask.vue";
 
-export default {
-  name: "TheDay",
-  components: {
-    TheTask
-  },
-  props: {
-    dayData: {
-      type: Object,
-      required: true
-    },
-  },
-  data() {
-    return {
-      tasks: this.dayData.tasks
-    }
-  },
-  methods: {
-    async deleteTask(pk) {
-      this.tasks = this.tasks.filter((task) => task.pk !== pk)
-      await fetch(`http://localhost:8000/my-tasks/${pk}`, {method: "DELETE"})
-    }
+<script setup>
+import {defineProps, ref} from "vue"
+
+import TheTask from "@/components/WeekTasksComponents/TheTask.vue";
+import {useWeekDataStore} from "@/store/weekData";
+import {useRouter} from "vue-router";
+import TheModalAdd from "@/components/WeekTasksComponents/TheModalAdd.vue";
+
+const props = defineProps(["dayData"])
+
+const weekDataStore = useWeekDataStore()
+
+const tasks = ref(props.dayData.tasks)
+const router = useRouter()
+
+const isModalVisible = ref(false)
+//
+const taskData = ref({
+  title: "",
+  description: "",
+  deadline: "",
+})
+
+function toggleForm() {
+  isModalVisible.value = !isModalVisible.value
+}
+
+function formClear() {
+  for (const key in taskData.value) {
+    taskData.value[key] = ""
   }
 }
+
+function addTask() {
+  weekDataStore.addTask(props.dayData.pk, taskData.value)
+  formClear()
+}
+
 </script>
 
-<style scoped>
 
+<style scoped>
+#overlay {
+  position: fixed;
+  opacity: 1;
+  transition: 0.2s ease-in-out;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.2);
+  pointer-events: all;
+}
 </style>
---
+++ frontend/src/components/WeekTasksComponents/TheModalAdd.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="modal-window active-modal" id="modal">
+    <h2>Add task for {{weekday}}</h2>
+    <div class="modal-header">
+      <button class="close-button" @click="$emit('close')">&times;</button>
+    </div>
+    <div class="modal-body">
+      <form ref="addForm" @submit.prevent="addTask">
+        <div>
+          <label for="title">Title</label>
+          <input required v-model="taskData.title" id="title" type="text">
+        </div>
+        <div>
+          <label for="description">Description</label>
+          <input v-model="taskData.description" id="description" type="text">
+        </div>
+        <div>
+          <label for="deadline">Deadline</label>
+          <input required v-model="taskData.deadline" id="deadline" type="time">
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const props = defineProps(["weekday"])
+
+const taskData = ref({
+  title: "",
+  description: "",
+  deadline: "",
+})
+</script>
+
+<style scoped>
+
+.modal-window {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(0);
+  border: 1px solid black;
+  transition: 0.2s ease-in-out;
+  border-radius: 10px;
+  z-index: 999;
+  background: white;
+  width: 500px;
+  max-width: 80%;
+}
+
+.modal-window.active-modal {
+  transform: translate(-50%, -50%) scale(1);
+}
+
+.modal-header {
+  padding: 10px 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid black;
+}
+
+.modal-header > .title {
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.modal-header > .close-button {
+  cursor: pointer;
+  border: none;
+  outline: none;
+  background: none;
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.modal-body {
+  padding: 10px 15px;
+  border-bottom: 1px solid black;
+}
+
+.modal-footer {
+  display: flex;
+  padding: 10px 15px;
+  justify-content: flex-end;
+}
+
+.button-cancel {
+  background: white;
+  border: rgb(64, 149, 191) 1px solid;
+  color: rgb(64, 149, 191);
+}
+
+.button-cancel:hover {
+  background: rgb(217, 234, 242);
+}
+
+
+</style>

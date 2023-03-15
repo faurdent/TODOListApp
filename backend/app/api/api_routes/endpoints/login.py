1474cd31d9ce4e3b62f09b7293d11056c5bb55d7
axeratos <nikitalukianitcares@gmail.com>
2023-03-15 11:24:38 +0200

    Login using pydantic schema

--- backend/app/api/api_routes/endpoints/login.py
+++ backend/app/api/api_routes/endpoints/login.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -9,20 +8,21 @@ from app.api.dependencies import get_db
 from app.core.security import verify_password, create_access_token
 from app.models import User
 from app.schemas import Token
+from app.schemas.user import UserCreate
 
 router = APIRouter()
 
 
 @router.post("/access-token", response_model=Token)
-async def login_access_token(db: AsyncSession = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    queryset = await db.execute(select(User).where(User.email == form_data.username))
+async def login_access_token(login_data: UserCreate, db: AsyncSession = Depends(get_db)):
+    queryset = await db.execute(select(User).where(User.email == login_data.email))
     user = queryset.scalars().first()
     if not user:
-        raise HTTPException(status_code=404, detail="User does not exists")
-    elif not verify_password(form_data.password, user.password):
-        raise HTTPException(status_code=400)
+        raise HTTPException(status_code=404, detail={"field": "username", "msg": "User does not exists"})
+    elif not verify_password(login_data.password, user.password):
+        raise HTTPException(status_code=400, detail={"field": "password", "msg": "Password is incorrect"})
 
-    access_token_expires = timedelta(minutes=30)
+    access_token_expires = timedelta(days=7)
     return {
         "access_token": create_access_token(user.pk, access_token_expires),
         "token_type": "bearer",

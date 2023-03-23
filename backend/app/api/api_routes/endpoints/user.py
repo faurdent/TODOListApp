12c84743eec4c9433449112ef6e20afc8f55d248
Axeratos <nikitalukianitcares@gmail.com>
2023-03-23 18:51:37 +0200

    Using pydantic schema during registration

--- backend/app/api/api_routes/endpoints/user.py
+++ backend/app/api/api_routes/endpoints/user.py
@@ -6,17 +6,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.dependencies import get_db
 from app.core.security import hash_password
 from app.models import User
+from app.schemas import UserCreate
 
 router = APIRouter()
 
 
 @router.post("/sign-up/")
-async def sign_up(db: AsyncSession = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    queryset = await db.execute(select(User).where(User.email == form_data.username))
+async def sign_up(user_data: UserCreate, db: AsyncSession = Depends(get_db)):
+    queryset = await db.execute(select(User).where(User.email == user_data.email))
     if queryset.scalars().first():
         raise HTTPException(status_code=400, detail="User with this email already exists")
-    new_user = User(email=form_data.username, password=hash_password(form_data.password))
+    new_user = User(email=user_data.email, password=hash_password(user_data.password))
     db.add(new_user)
-    await db.commit()
+    await db.flush()
     await db.refresh(new_user)
     return new_user

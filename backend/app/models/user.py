256ceef2df7352a844cf65d3533c15aa9fd6ee13
Axeratos <nikitalukianitcares@gmail.com>
2023-03-29 20:34:07 +0300

    New accounts is not verified by default

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -7,4 +7,4 @@ class User(Base):
     __tablename__ = "user"
     email: Mapped[str]
     password: Mapped[str]
-    is_verified: Mapped[bool] = mapped_column(default=True)
+    is_verified: Mapped[bool] = mapped_column(default=False)

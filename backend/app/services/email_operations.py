f39f9ee9ff21f028390ff37471662a2310212ddf
Axeratos <nikitalukianitcares@gmail.com>
2023-04-02 12:40:49 +0300

    Template for verification email updated

--- backend/app/services/email_operations.py
+++ backend/app/services/email_operations.py
@@ -1,13 +1,14 @@
 from fastapi_mail import FastMail, MessageSchema, MessageType
 
-from app.crud import verification_code
 from app.core.config import connection_config
+from app.crud import verification_code
 
 
 async def send_verification_code(db, user_id, email):
     verification_user_code = await verification_code.create_or_update_verification_code(db, user_id)
     fast_mail = FastMail(connection_config)
-    message_body = f"<a href='http://localhost:8000/users/" \
+    message_body = f"<p>Your code: {verification_user_code.verification_code}</p>" \
+                   f"<a href='http://localhost:8000/users/" \
                    f"verify/{verification_user_code.verification_code}'>Verify account</a>"
     message = MessageSchema(
         subject="Account verification",

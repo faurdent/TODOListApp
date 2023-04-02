8eb4639982f873cfbf523a938c876ffdbae72ad2
Axeratos <nikitalukianitcares@gmail.com>
2023-04-02 12:41:47 +0300

    Write uuid as str

--- backend/app/crud/crud_verification_code.py
+++ backend/app/crud/crud_verification_code.py
@@ -1,5 +1,5 @@
-from datetime import datetime, timedelta
 import uuid
+from datetime import datetime, timedelta
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -9,12 +9,12 @@ from app.models import VerificationCode
 
 class CRUDVerificationCode(CRUDBase[VerificationCode]):
     async def create_or_update_verification_code(self, db: AsyncSession, user_id: int):
-        new_code = uuid.uuid4()
+        new_code = str(uuid.uuid4())
         valid_until = datetime.now() + timedelta(minutes=5)
         code_obj = await self.get(db, user_id=user_id)
         if code_obj:
             update_data = {"verification_code": new_code, "valid_until": valid_until}
-            return await self.update(db, code_obj, update_data)
+            return await self.update(db, update_data, code_obj)
         new_code_obj = self.model(verification_code=new_code, valid_until=valid_until, user_id=user_id)
         db.add(new_code_obj)
         await db.flush()

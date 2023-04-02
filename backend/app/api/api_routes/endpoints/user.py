e4a2e7bcb3611615dcbe61290f2ddbe935887b78
Axeratos <nikitalukianitcares@gmail.com>
2023-04-02 12:46:19 +0300

    Endpoint for email verification

--- backend/app/api/api_routes/endpoints/user.py
+++ backend/app/api/api_routes/endpoints/user.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
-from fastapi.responses import JSONResponse
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,7 +6,7 @@ from app.api.dependencies import get_db
 from app.core.security import hash_password
 from app.crud import user, verification_code
 from app.models import User
-from app.schemas import UserCreate
+from app.schemas import UserCreate, UserSchema
 from app.services.email_operations import send_verification_code
 
 router = APIRouter()
@@ -20,7 +19,6 @@ async def sign_up(user_data: UserCreate, background_tasks: BackgroundTasks, db: 
         raise HTTPException(status_code=400, detail="User with this email already exists")
     new_user_object = User(email=user_data.email, password=hash_password(user_data.password))
     new_user = await user.create(db, new_user_object)
-    background_tasks.add_task(send_verification_code, db, new_user.pk, new_user.email)
     return new_user
 
 
@@ -28,6 +26,14 @@ async def sign_up(user_data: UserCreate, background_tasks: BackgroundTasks, db: 
 async def verify_account(verification_user_code: str, db: AsyncSession = Depends(get_db)):
     verification_code_obj = await verification_code.get(db, verification_code=verification_user_code)
     if not verification_code_obj:
-        return
-    await user.activate_account(db, verification_code_obj.user_id)
-    return JSONResponse(status_code=200, content={"msg": "Account verified successfully"})
+        raise HTTPException(status_code=400, detail="Incorrect verification code")
+    verified_user_obj = await user.activate_account(db, verification_code_obj.user_id)
+    return verified_user_obj
+
+
+@router.post("/send-verification-email")
+async def send_verification_email(
+        user_info: UserSchema, background_tasks: BackgroundTasks, db: AsyncSession = Depends(get_db)
+):
+    background_tasks.add_task(send_verification_code, db, user_info.pk, user_info.email)
+    return {"msg": "Email sent successfully"}

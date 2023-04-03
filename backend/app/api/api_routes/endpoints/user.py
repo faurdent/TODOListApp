2ca30fbbb6f3eb1d22aba906b5cd28c27263e6a3
axeratos <nikitalukianitcares@gmail.com>
2023-04-03 17:55:57 +0300

    Send email after registration. Separate endpoint for logged in users

--- backend/app/api/api_routes/endpoints/user.py
+++ backend/app/api/api_routes/endpoints/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.dependencies import get_db
+from app.api.dependencies import get_db, get_current_user
 from app.core.security import hash_password
 from app.crud import user, verification_code
 from app.models import User
@@ -19,6 +19,7 @@ async def sign_up(user_data: UserCreate, background_tasks: BackgroundTasks, db: 
         raise HTTPException(status_code=400, detail="User with this email already exists")
     new_user_object = User(email=user_data.email, password=hash_password(user_data.password))
     new_user = await user.create(db, new_user_object)
+    background_tasks.add_task(send_verification_code, db, new_user.pk, new_user.email)
     return new_user
 
 
@@ -27,13 +28,14 @@ async def verify_account(verification_user_code: str, db: AsyncSession = Depends
     verification_code_obj = await verification_code.get(db, verification_code=verification_user_code)
     if not verification_code_obj:
         raise HTTPException(status_code=400, detail="Incorrect verification code")
-    verified_user_obj = await user.activate_account(db, verification_code_obj.user_id)
-    return verified_user_obj
+    return await user.activate_account(db, verification_code_obj.user_id)
 
 
 @router.post("/send-verification-email")
 async def send_verification_email(
-        user_info: UserSchema, background_tasks: BackgroundTasks, db: AsyncSession = Depends(get_db)
+        background_tasks: BackgroundTasks,
+        db: AsyncSession = Depends(get_db),
+        user_info: UserSchema = Depends(get_current_user)
 ):
     background_tasks.add_task(send_verification_code, db, user_info.pk, user_info.email)
     return {"msg": "Email sent successfully"}
